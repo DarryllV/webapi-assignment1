6ccd8beb69b4e7812d545061d1a2b62f915c13d7
DarryllV <dondarryll8@gmail.com>
2024-11-15 17:56:41 +0800

    added placeholder code

---
+++ Darryll_TT-Service.js
@@ -0,0 +1,33 @@
+let timetabling = {
+
+    students:[],
+    tutors:[],
+
+
+    // Add tutors
+    AddTutor() {
+        
+
+    },
+
+    // Add Students
+    AddStudent() {
+        
+
+    }
+
+    //Schedule
+
+
+
+    //Show schedule
+
+    
+
+
+    //Edit schedule
+
+}
+
+
+module.exports = timetabling;
